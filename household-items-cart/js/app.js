fd05144f7247e40d6d0c56ceb1b7bebb94c4bc87
0x4ngK4n <joy1337@protonmail.com>
2021-12-02 21:52:50 +0530

    Added dummy XHR requests to user actions, fixed rowID count after delete action

--- household-items-cart/js/app.js
+++ household-items-cart/js/app.js
@@ -8,15 +8,16 @@ class UI {
     this.itemForm = document.getElementById('item-form');
     this.itemList = [];
     this.itemID = 0;
-    this.rowID = 0;
+    this.rowNumber = 0;
   }
   // click add item '+' button
-  addGoodDetails() {
+  addGoodDetails(rowid) {
     // when user clicks, I add new item rew to the DOM
     // perfrom form validation
-    let tmp = 'row_'+this.rowID;
-    console.log(tmp);
-    if (document.getElementById(tmp).children[1].children[0].value === "" || document.getElementById(tmp).children[2].children[0].value === "" || document.getElementById(tmp).children[3].value === "Item quantity" ) {
+    const currentItemName = document.getElementById(rowid).children[1].children[0].value;
+    const currentItemWeight = document.getElementById(rowid).children[2].children[0].value;
+    const currentItemQuantity = document.getElementById(rowid).children[3].value;
+    if (currentItemName === "" || currentItemWeight === "" || currentItemQuantity === "Item quantity" ) {
       // validation fails then show the error
       this.itemFeedback.classList.add('showItem')
       this.itemFeedback.innerHTML = `<p> Item values cannot be empty or imvalid</p>`;
@@ -28,7 +29,7 @@ class UI {
       div.classList.add('form-row')
       div.innerHTML = `
         <div class="col-xs-1">
-          <input class="form-control item-input" type="checkbox" checked="checked" value="" id="item-status">
+          <input class="form-control item-input" type="checkbox" value="" id="item-status">
         </div>
         <div class="col-sm">
           <input type="text" class="form-control item-input" id="item-name" placeholder="Item name">
@@ -47,42 +48,77 @@ class UI {
       <button type="submit" class="btn text-capitalize add-good" id="add-good-submit">+</button>
       <button type="submit" class="btn text-capitalize remove-good" id="remove-good-submit">-</button>
      `;
-     this.rowID++;
-     div.setAttribute('id','row_'+this.rowID);
+     this.rowNumber++;
+     div.setAttribute('id','row_'+this.rowNumber);
      this.itemForm.appendChild(div);
      let items = {
       id: this.itemID,
-      name: this.itemName.value,
-      weight: this.itemWeight.value,
-      quantity: this.itemQuantity.value
+      name: currentItemName,
+      weight: currentItemWeight,
+      quantity: currentItemQuantity
     }
     this.itemID++;
     this.itemList.push(items);
-    console.log(this.itemList);
+    //console.log(this.itemList);
     }
   }
   // click remove item '-' button
   removeGoodDetails(id) {
     if (id !== "row_0") {
       let rownumber = id.split("_")[1];
-      // decrement rowID after every deletion
-      rownumber -= 1;
+      // just remove the element from dom
       var toRemoveRow = document.getElementById(id);
       toRemoveRow.remove();
-      this.rowID = rownumber;
-      // ToDo delete the itemList as well
+      console.log(this.itemList[rownumber]);
+      this.makeRequest("delete",this.itemList[rownumber]);
+      // delete the element in itemList as well
+      this.itemList.splice(rownumber,1);
     }
   }
   // submit item form via 'Order' button
   submitItemForm() {
-    console.log("submit goods");
+    this.makeRequest("submit", this.itemList);
+  }
+  // debug: skeletal function; needs refining
+  makeRequest(type, obj) {
+    if (type === "submit") {
+      let xhr = new XMLHttpRequest();
+      // debug placeholder URL
+      let url = "http://127.0.0.1:8080/submit";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      // Create a state change callback
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Print received data from server
+            result.innerHTML = this.responseText;
+        }
+      };
+      xhr.send(JSON.stringify(obj))
+    }
+    else if (type === "delete") {
+      let xhr = new XMLHttpRequest();
+      // debug placeholder URL
+      let url = "http://127.0.0.1:8080/delete";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      // Create a state change callback
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Print received data from server
+            result.innerHTML = this.responseText;
+        }
+      };
+      xhr.send(JSON.stringify(obj))
+    }
   }
 }
 
 function eventListeners() {
 // when clicking on '+' or '-' button
-//const addGood = document.getElementById('add-good-submit')
 const modifyGood = document.getElementById('item-form')
+// when changing item status
+const modifyGoodStatus = document.getElementById('item-status')
 // when submitting via'Orders' button
 const itemSubmit = document.getElementById('item-submit')
 
@@ -94,17 +130,25 @@ modifyGood.addEventListener('click', function(event){
   event.preventDefault();
   // reference: https://typeofnan.dev/how-to-bind-event-listeners-on-dynamically-created-elements-in-javascript/
   if (event.target.textContent === "+") {
-    ui.addGoodDetails();
+    ui.addGoodDetails(event.target.parentElement.id);
   }
   if (event.target.textContent === "-") {
     ui.removeGoodDetails(event.target.parentElement.id);
   }
 });
-itemSubmit.addEventListener('submit', function(event){
+
+modifyGoodStatus.addEventListener('click', function(event){
+  console.log('click');
+});
+
+itemSubmit.addEventListener('click', function(event){
+  console.log("pressed");
   ui.submitItemForm();
 });
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
+  console.log("hi there")
   eventListeners();
 });
